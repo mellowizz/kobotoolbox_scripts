ba5310ac7b783c9972dac2e3a0f453ca071a7982
Niklas Moran <niklas@niklasmoran.com>
2018-02-09 16:14:20 -0500

    working version

--- inventory_upload.py
+++ inventory_upload.py
@@ -4,30 +4,26 @@ import json
 
 
 base_url = "https://kc.kobotoolbox.org/api/v1/"
-
-data_url = base_url + 'data/'
 user = input("Please enter your username: ")
 password = input("Please enter your password: ")
 formname = input("Please enter your formname: ")
-# fileloc = input("Please enter the path of the file you want to upload: ")
+fileloc = input("Please enter the path of the file you want to upload: ")
+form_url = base_url + 'forms?owner{}'.format(user)
 pk = 0
 with requests.Session() as s:
-    # r = s.get(base_url, auth=(user, password))
-    form_url = base_url + "/forms?owner={}".format(user)
     r = s.get(form_url, auth=(user, password))
     print(r)
     data = json.loads(r.text)
     for i in data:
-        #print(('Name: {} formid: {}'.format(i['title'], i['formid'])))
+        print(('Name: {} formid: {}'.format(i['title'], i['formid'])))
         if i['title'] == formname or i['title'].lower() == formname.lower():
             pk = i['formid']
             print('Found: {}, id is: {}'.format(i['title'], i['formid']))
             break;
     if pk == 0:
         print('ERROR: formname not found!')
-    else:
-        upload_url = data_url + str(pk) + "/csv_import"
-        print('url: {}'.format(upload_url))
-    # files = {'csv_file': open(fileloc, 'rb')}
-    # r = s.post(form_url, files=files, auth=(user, password))
-    # r.raise_for_status()
+    upload_url = base_url + 'forms/' + str(pk) + '/csv_import'
+    print('url: {}'.format(upload_url))
+    files = {'csv_file': open(fileloc, 'rb')}
+    r = s.post(upload_url, files=files, auth=(user, password))
+    r.raise_for_status()
