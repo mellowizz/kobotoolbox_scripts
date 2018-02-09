5cc28f7b29aed941896ae85dbd1e3011786b4718
Niklas Moran <niklas@niklasmoran.com>
2018-02-09 13:50:13 -0500

    changes

--- inventory_upload.py
+++ inventory_upload.py
@@ -1,4 +1,3 @@
-
 import requests
 import sys
 import json
@@ -11,29 +10,24 @@ user = input("Please enter your username: ")
 password = input("Please enter your password: ")
 formname = input("Please enter your formname: ")
 # fileloc = input("Please enter the path of the file you want to upload: ")
-pk = ''
+pk = 0
 with requests.Session() as s:
     # r = s.get(base_url, auth=(user, password))
     form_url = base_url + "/forms?owner={}".format(user)
     r = s.get(form_url, auth=(user, password))
     print(r)
     data = json.loads(r.text)
-    print('data = {}'.format(data))
-    for i, j in enumerate(data):
-        print('{}: formname:{} formid: {}'.format(i, j['formtitle'], j['formid']))
-        pk = j['formid']
-        #print('Found- Name: {} formid: {}'.format(i['title'], i['formid']))
-        #break
-    if pk == '' or pk is None:
-        print("ERROR: PK is null!")
-    else:   
+    for i in data:
+        #print(('Name: {} formid: {}'.format(i['title'], i['formid'])))
+        if i['title'] == formname or i['title'].lower() == formname.lower():
+            pk = i['formid']
+            print('Found: {}, id is: {}'.format(i['title'], i['formid']))
+            break;
+    if pk == 0:
+        print('ERROR: formname not found!')
+    else:
         upload_url = data_url + str(pk) + "/csv_import"
         print('url: {}'.format(upload_url))
-    '''
-    r = s.get(base_url + 'data?owner{}'.format(user), auth=(user, password))
-    print(r)
-    print(r.text)
-    files = {'csv_file': open('coh-field-upload.csv', 'rb')}
-    r = s.post(form_url, files=files, auth=(user, password))
-    r.raise_for_status()
-    '''
+    # files = {'csv_file': open(fileloc, 'rb')}
+    # r = s.post(form_url, files=files, auth=(user, password))
+    # r.raise_for_status()
--- list_inventory_id.py
+++ list_inventory_id.py
@@ -4,15 +4,23 @@ import json
 
 base_url = "https://kc.kobotoolbox.org/api/v1/"
 
-data_url = base_url + 'data' 
+data_url = base_url + 'data'
 form_url = base_url + 'forms'
 user = input("Please enter your username: ")
 password = input("Please enter your password: ")
 formname = input("Please enter your formname: ")
+pk = 0
 with requests.Session() as s:
     r = s.get(form_url + '?owner{}'.format(user), auth=(user, password))
     print(r)
     data = json.loads(r.text)
     for i in data:
+        # print(i.keys())
         print(('Name: {} formid: {}'.format(i['title'], i['formid'])))
+        if i['title'] == formname or i['title'].lower() == formname.lower():
+            pk = i['formid']
+            print('Found formname: {}, id is: {}'.format(i['title'], i['formid']))
+    if pk == 0:
+        print('ERROR: formname not found!')
     r.raise_for_status()
+
